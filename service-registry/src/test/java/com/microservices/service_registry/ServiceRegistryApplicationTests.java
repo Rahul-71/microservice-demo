79d723ded2e79b10a4e129a2a7bcf3e30b9418b7
Rahul-71 <rahultiwari.201307@gmail.com>
2025-02-15 19:36:21 +0530

    configured service-registry

--- service-registry/src/test/java/com/microservices/service_registry/ServiceRegistryApplicationTests.java
+++ service-registry/src/test/java/com/microservices/service_registry/ServiceRegistryApplicationTests.java
@@ -1,13 +1,13 @@
-package com.microservices.service_registry;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ServiceRegistryApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.microservices.service_registry;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ServiceRegistryApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
